75661c6a345d67506712c80976ff8d33324b3e57
Manel Romero Garcia <manel.romero.garcia@gmail.com>
2017-12-17 13:54:18 +0100

    Crear clase prueba

---
+++ UF6_Practica_Aerolinies_Manel_Ruben/src/uf6_practica_aerolinies_manel_ruben/UF6_Practica_Aerolinies_Manel_Ruben.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uf6_practica_aerolinies_manel_ruben;
+
+/**
+ *
+ * @author Manel
+ */
+public class UF6_Practica_Aerolinies_Manel_Ruben {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("");
+    }
+    
+}
