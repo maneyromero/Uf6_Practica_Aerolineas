387dc84d2b6f580e69e317fbfe6d4ee20a19a9f3
rubenniki <rubenniki@gmail.com>
2017-12-17 13:44:03 +0100

    Merge origin/master

---
+++ UF6_Aerolinies_Manel_Ruben/src/uf6_aerolinies_manel_ruben/UF6_Aerolinies_Manel_Ruben.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uf6_aerolinies_manel_ruben;
+
+/**
+ *
+ * @author ruben
+ */
+public class UF6_Aerolinies_Manel_Ruben {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+             String url="jdbc:mysql://localhost:3306/uf6_practica_aerolineas";
+       String username="root";
+       String password="";
+       String query="select * from aerolinies;";
+    }
+    
+}
