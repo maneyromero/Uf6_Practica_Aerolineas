d5fe77cb9bf4943e18b8c6745493ebf5be0fee74
rubenniki <rubenniki@gmail.com>
2017-12-19 08:43:46 +0100

    Merge origin/master

---
+++ UF6_Practica_Aerolinies_Manel_Ruben/src/uf6_practica_aerolinies_manel_ruben/Utilities/ConnectDB.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uf6_practica_aerolinies_manel_ruben;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import javax.activation.CommandInfo;
+
+/**
+ *
+ * @author Manel
+ */
+public class ConnectDB {
+   private static Connection connection;
+   
+   private ConnectDB(){
+       
+   }
+   
+   public static Connection openConnection() {
+       if(connection==null){
+           connection=DriverManager.getConnection(url, user, password);
+           System.out.println("Open database");
+       }
+       return connection;
+   }
+   
+   public static void closeConnection(){
+       if(connection!=null){
+           connection.close();
+           connection=null;
+           System.out.println("Database closed");
+       }
+   }
+    
+   
+}
