f0b756663e7acd00d2ed24204f8836eae55ad57d
Manel Romero Garcia <manel.romero.garcia@gmail.com>
2017-12-19 18:52:58 +0100

    main

--- UF6_Practica_Aerolinies_Manel_Ruben/src/Test/UF6_Practica_Aerolinies_Manel_Ruben.java
+++ UF6_Practica_Aerolinies_Manel_Ruben/src/Test/UF6_Practica_Aerolinies_Manel_Ruben.java
@@ -5,11 +5,17 @@
  */
 package Test;
 
+import Dao.AerolineaDAO;
+import Dao.AeropuertoDAO;
+import Dao.AvionDAO;
+import Dao.DAOFactory;
+import Dao.PassajeroDAO;
+import Dao.TicketDAO;
+import Model.Passajero;
+import Utilities.ConnectDB;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Scanner;
 
 /**
  *
@@ -20,26 +26,70 @@ public class UF6_Practica_Aerolinies_Manel_Ruben {
     /**
      * @param args the command line arguments
      */
-    private static String nombre;
+    private static String nombre,DNI,apellido1,apellido2,codigo_avion;
+    private static int edad;
 
     public static void main(String[] args) {
         // TODO code application logic here
-        MYSQLDBConnection.url = "jdbc:mysql://localhost:3306/uf6_practica_aerolineas";
-        MYSQLDBConnection.username = "root";
-        MYSQLDBConnection.password = "";
-        String query = "select * from aerolineas;";
-        try (Connection con = DriverManager.getConnection(url, username, password);
-                Statement statement = con.createStatement();
-                ResultSet rs = statement.executeQuery(query);) {
-            while (rs.next()) {
-                nombre = rs.getString("nombre");
-                System.out.println("Nombre aerolinea: " + nombre);
-            }
-            con.close();
+        DAOFactory factory = new DAOFactory();
+        AeropuertoDAO aeropueto;
+        AerolineaDAO aerolinea;
+        AvionDAO avion;
+        PassajeroDAO passajero = null;
+        TicketDAO ticket;
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+        
+        try {
+            Connection con=ConnectDB.openConnection();
+            do {
+                escribirMenu();
+                num = sc.nextInt();
+                switch (num) {
+                    case 1:
+                        String query="SELECT v.codigo_avion FROM aviones as v WHERE codigo_aerolinea_fk in (SELECT l.codigo_aerolinea from aerolineas as l where l.codigo_aeropuerto_fk like (SELECT codigo_aeropuerto from aeropuertos WHERE nombre like '"+codigo_avion+"'))";
+                        System.out.println("Introduce el destino");
+                        codigo_avion=sc.next();
+                        System.out.println("Introduce su DNI");
+                        DNI=sc.next();
+                        System.out.println("Introduce el nombre");
+                        nombre=sc.next();
+                        System.out.println("Introduce el primer apellido");
+                        apellido1=sc.next();
+                        System.out.println("Introduce el segundo apellido");
+                        apellido2=sc.next();
+                        System.out.println("Introduce la edad");
+                        edad=sc.nextInt();
+                        Passajero p=new Passajero(DNI, codigo_avion, nombre, apellido1, apellido2, edad);
+                        passajero.addPasajero(p, con);
+                        con.close();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        System.out.println("Exit");
+                        break;
+                    default:
+                        break;
+
+                }
+            } while (num != 5);
         } catch (SQLException ex) {
 
         }
 
+    }
+
+    private static void escribirMenu() {
+        System.out.println("-----Menu-----");
+        System.out.println("1- Añadir Passajero\n"
+                + "2- Buscar avion de un vuelo\n"
+                + "3- Hacer billete de vuelo\n"
+                + "4- Listar passajeros");
     }
 
 }
